68ced840fdc1024cd423175418ac01728d927ec6
alcon <alconfw@mail.ru>
2025-07-08 14:08:44 +0300

    Made tasks 1 and 2

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace TaskManager;
+﻿using Models;
+
+namespace TaskManager;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        
+        Job job = new Job();
+        job.ChangeCategory(JobCategory.Work);
+        job.Title = "test";
+        job.AddTag("TAGTAGTAG");
+        Console.WriteLine(job);
     }
 }
