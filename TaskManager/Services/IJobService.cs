ef18470111831fa44adf124fbef94cd344d18754
alcon <alconfw@mail.ru>
2025-10-31 15:48:31 +0300

    fixed the issues and complited the method

--- TaskManager/Services/IJobService.cs
+++ TaskManager/Services/IJobService.cs
@@ -8,8 +8,8 @@ public interface IJobService
     List<Job> GetAllJobs();
     Job? GetJobById(Guid id);
     void AddJob(Job job);
-    void UpdateJob(Guid id, out Job updatedJob);
-    void DeleteJob(Job job);
+    bool UpdateJob(Guid id, Job updatedJob);
+    void DeleteJob(Guid id);
     List<Job> GetJobByStatus(JobStatus status);
     Task SaveToFile();
     Task<List<Job>> LoadFromFile();
--- TaskManager/Services/JobService.cs
+++ TaskManager/Services/JobService.cs
@@ -1,16 +1,19 @@
 ﻿using Models;
 
 namespace Services;
+
 /// <summary>
 /// класс реализующий IJobService
 /// </summary>
 public class JobService : IJobService
 {
     private readonly List<Job> _jobs;
+
     public JobService()
     {
         _jobs = new List<Job>();
     }
+
     /// <summary>
     /// возвращает список всех задач
     /// </summary>
@@ -19,74 +22,88 @@ public class JobService : IJobService
     {
         return _jobs.ToList();
     }
-/// <summary>
-/// вернет задачу по уникальному номеру
-/// </summary>
-/// <param name="id">уникальный номер задачи</param>
-/// <returns>задачу</returns>
+
+    /// <summary>
+    /// вернет задачу по уникальному номеру
+    /// </summary>
+    /// <param name="id">уникальный номер задачи</param>
+    /// <returns>задачу</returns>
     public Job? GetJobById(Guid id)
     {
         return _jobs.FirstOrDefault(j => j.Id.Equals(id));
     }
-/// <summary>
-/// добавляет в список задачу
-/// </summary>
-/// <param name="job">указывает какую задачу надо добавить в список</param>
-/// <exception cref="ArgumentNullException">если задача нул, вадет ошибку</exception>
-/// <exception cref="ArgumentException">нельзя добавить уже существующую задачу</exception>
+
+    /// <summary>
+    /// добавляет в список задачу
+    /// </summary>
+    /// <param name="job">указывает какую задачу надо добавить в список</param>
+    /// <exception cref="ArgumentNullException">если задача нул, вадет ошибку</exception>
+    /// <exception cref="ArgumentException">нельзя добавить уже существующую задачу</exception>
     public void AddJob(Job job)
     {
         if (job == null)
         {
-            throw new ArgumentNullException();
+            throw new ArgumentNullException(nameof(job));
+        }
+
+        if (job.Id == Guid.Empty)
+        {
+            throw new ArgumentException("Задачи с пустым Id не должно быть!", nameof(job));
         }
 
         if (_jobs.Any(j => j.Id == job.Id))
         {
-            throw new ArgumentException("Задача с таким номером уже существует!");
+            throw new ArgumentException("Задача с таким номером уже существует!", nameof(job));
         }
+
         _jobs.Add(job);
+
     }
-/// <summary>
-/// обновляет задачу из списка задач
-/// </summary>
-/// <param name="id">уникальный номер задачи котору надо обновить</param>
-/// <param name="updatedJob">обновленная задача</param>
-/// <exception cref="ArgumentNullException">если задачи нет, ошибка</exception>
-    public void UpdateJob(Guid id, out Job updatedJob)
+
+    /// <summary>
+    /// Пытается обновить задачу с указанным ID
+    /// </summary>
+    /// <returns>true если задача была обновлена, false если задача не найдена</returns>
+
+    public bool UpdateJob(Guid id, Job updatedJob)
     {
-        var currentJob = _jobs.FirstOrDefault(j => j.Id == id);
-        if (currentJob == null)
+        if (updatedJob == null)
         {
-            throw new ArgumentNullException($"Задачи с {id} Id не существует");
+            throw new ArgumentNullException(nameof(updatedJob));
         }
 
-        
-    }
-/// <summary>
-/// удаляет задач из списка задач
-/// </summary>
-/// <param name="job">задача которую надо удалить</param>
-/// <exception cref="ArgumentNullException">если задача пуста, ошибка</exception>
-    public void DeleteJob(Job job)
-    {
-        if (job == null)
+        var jobYoUpdateIndex = _jobs.FindIndex(j => j.Id == id);
+        if (jobYoUpdateIndex == -1)
         {
-            throw new ArgumentNullException("Такой задачи не существует");
+            return false;
         }
 
-        var jobToDelete = _jobs.FirstOrDefault(j => j.Id == job.Id);
-        if (job is not null)
-        {
-            _jobs.Remove(jobToDelete);
-           
-        }
+        _jobs[jobYoUpdateIndex] = updatedJob;
+        return true;
     }
-/// <summary>
-/// возращает копию списка задач по статусу
-/// </summary>
-/// <param name="status">задачи с таким статусом будут возвращены</param>
-/// <returns></returns>
+
+    /// <summary>
+    /// Удаляет задачу по уникальному Id
+    /// </summary>
+    /// <param name="id">параметр по которому будет производиться поиск задачи</param>
+    /// <exception cref="ArgumentNullException">задача не найдена</exception>
+    public void DeleteJob(Guid id)
+    {
+        var jobToDelete = _jobs.FirstOrDefault(j => j.Id == id);
+        if (jobToDelete == null)
+        {
+            throw new InvalidOperationException("Задача не должна быть пустой!");
+        }
+
+        _jobs.Remove(jobToDelete);
+
+    }
+
+    /// <summary>
+    /// возращает копию списка задач по статусу
+    /// </summary>
+    /// <param name="status">задачи с таким статусом будут возвращены</param>
+    /// <returns></returns>
     public List<Job> GetJobByStatus(JobStatus status)
     {
         return _jobs.Where(s => s.Status == status).ToList();
