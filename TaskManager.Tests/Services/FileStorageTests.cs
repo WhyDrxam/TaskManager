70c7c74c56b2307eddc06a81051b25ed386acd2b
alcon <alconfw@mail.ru>
2025-11-20 10:04:32 +0300

    changes

---
+++ TaskManager.Tests/Services/FileStorageTests.cs
@@ -0,0 +1,15 @@
+﻿// using Services;
+//
+// namespace TaskManager.Tests.Services.Tests;
+//
+// public class FileStorageTests
+// {
+//     private const string VALID_FILE_PATH =
+//     //лист нул а путь правильный
+//     [Fact]
+//     public void SaveTasks_ListofJobsNull_ThrowsArgumentNullExeption()
+//     {
+//         var fileStorage = new FileStorage();
+//         //fileStorage.SaveTasks(null,);
+//     }
+// }
--- TaskManager/Services/FileStorage.cs
+++ TaskManager/Services/FileStorage.cs
@@ -6,8 +6,17 @@ namespace Services;
 
 public class FileStorage : IFileStorage
 {
+    JsonSerializerOptions options = new()
+    {
+        PropertyNameCaseInsensitive = true,
+        IncludeFields = true,
+        WriteIndented = true
+    };
     public void SaveTasks(List<Job> tasks, string filePath)
     {
+        
+        
+        var extension = Path.GetExtension(filePath);
         if (tasks ==  null || tasks.Count <= 0 )
         {
             throw new ArgumentNullException("Список не должен быть пустым!", nameof(tasks));
@@ -17,15 +26,25 @@ public class FileStorage : IFileStorage
         {
             throw new ArgumentNullException("Путь к папке не должен быть пустым", nameof(filePath));
         }
+
+        if (extension != ".json")
+        {
+            throw new ArgumentException("Неверный формат файла, должен быть .json", nameof(extension));
+        }
         
         using (var stream = File.Open(filePath, FileMode.OpenOrCreate))
         {
-            JsonSerializer.Serialize(stream, tasks);
+            JsonSerializer.Serialize(stream, tasks, options);
         }
     }
 
     public List<Job> LoadTasks(string filePath)
     {
+        var extension = Path.GetExtension(filePath);
+        if (extension != ".json")
+        {
+            throw new ArgumentException("Неверный формат файла, должен быть .json", nameof(extension));
+        }
         if (string.IsNullOrWhiteSpace(filePath))
         {
             throw new ArgumentNullException("Путь к папке не должен бть пустым", nameof(filePath));
@@ -37,7 +56,7 @@ public class FileStorage : IFileStorage
         }
 
         var fReader = File.ReadAllText(filePath);
-        var content = JsonSerializer.Deserialize<List<Job>>(fReader); 
+        var content = JsonSerializer.Deserialize<List<Job>>(fReader, options); 
         return content ?? [];
     }
 }
